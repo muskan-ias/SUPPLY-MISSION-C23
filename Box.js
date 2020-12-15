ca216b0c075b40d2621b90279fcb548c1ead7134
muskan-ias <73595142+muskan-ias@users.noreply.github.com>
2020-12-15 20:53:22 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box {
+
+constructor(x,y,width,height){
+var box_options = {
+isStatic : true
+
+
+}
+this.body = Bodies.rectangle(x,y,width,height,box_options)
+this.width = width
+this.height = height
+World.add(world,this.body)
+
+}
+
+display () {
+var pos = this.body.position
+fill("red")
+rect(pos.x,pos.y,this.width,this.height)
+}
+
+
+
+}
+
+
